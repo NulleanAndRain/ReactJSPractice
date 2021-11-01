50a4d361f2e28ba5cf6e7420f24f7758283f5ca5
Nullean <mr000lean@gmail.com>
2021-11-01 17:16:04 +0400

    changed table header cells

--- src/components/Table/TableHeader.js
+++ src/components/Table/TableHeader.js
@@ -1,9 +1,20 @@
 import { TableItem } from "./TableItem";
 
-export function TableHeader(props) {
+const TableHeader = (props) => {
     return <TableItem 
-        Item1 = {<>Name</>}
-        Item2 = {<>Price</>}
+        Item1 = {<CtrlCell text = 'Name'/>}
+        Item2 = {<CtrlCell text = 'Price'/>}
         Item3 = {<>Actions</>}
     />
 }
+
+const CtrlCell = props =>{
+    return (
+        <div className = 'HeaderCell'>
+            <span className='HeaderCellText'>{props.text}</span>
+            <button onClick = {props.action}> </button>
+        </div>
+    );
+}
+
+export {TableHeader};
