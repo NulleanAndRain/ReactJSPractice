6045d80b18689391b4000f03131e709c43a47cb9
Nullean <mr000lean@gmail.com>
2021-11-03 14:08:32 +0400

    added useMemo in table items

--- src/components/Table/TableItem.js
+++ src/components/Table/TableItem.js
@@ -2,12 +2,13 @@ import { ItemActions } from './ItemActions';
 import { TableRow } from './TableRow';
 import './Table.css';
 import { NameCell } from './NameCell';
+import { useMemo } from 'react';
 
 export function TableItem(props) {
-    return (
+    return useMemo(() =>
     <TableRow 
         Item1 = {<NameCell name = {props.name} count = {props.count} />}
         Item2 = {'$' + props.price}
         Item3 = {<ItemActions />}
-    />);
+    />, [props]);
 }
